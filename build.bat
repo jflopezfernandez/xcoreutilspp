c0a9e4abda420de95032be2f3c291519001f29a2
lopezfjose <jflopezfernandez@gmail.com>
2019-02-25 23:38:04 -0500

    Modified build.bat; need to add boost include directories

--- build.bat
+++ build.bat
@@ -1,6 +1,7 @@
-@ECHO OFF
+@ECHO ON
 
 mkdir xcoreutils-build
 cd xcoreutils-build
-cmake -G "Visual Studio 15 2017 Win64" -DCMAKE_TOOLCHAIN_FILE="C:/vcpkg/scripts/buildsystems/vcpkg.cmake" -Thost=x64 . --target ../xcoreutils
+cmake -G "Visual Studio 15 2017 Win64" -DCMAKE_TOOLCHAIN_FILE="C:/vcpkg/scripts/buildsystems/vcpkg.cmake" -Thost=x64 . --target ..
 cmake --build .
+cd ..
